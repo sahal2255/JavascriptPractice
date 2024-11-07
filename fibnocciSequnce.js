cf0de87cb11d19d3cdcd52c4a57bc8595cac1c37
sahal2255 <sahalvv9656@gmail.com>
2024-11-07 21:58:51 +0530

    fibnocci

--- fibnocciSequnce.js
+++ fibnocciSequnce.js
@@ -1,3 +1,6 @@
+
+`fibnocci sequence is the sequence of numbers in which each number in the sequnce is the sum of the two number before it`
+
 function fibnocci(n){
     let fib=[0,1]
     for(i=2;i<n;i++){
