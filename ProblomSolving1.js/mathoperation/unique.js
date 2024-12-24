90849d2a22f9e4518e1be0c8c0ab9d40b88600d1
sahal2255 <sahalvv9656@gmail.com>
2024-12-24 22:02:08 +0530

    unique value

---
+++ ProblomSolving1.js/mathoperation/unique.js
@@ -0,0 +1,15 @@
+const array=['apple','banana','orange','apple','watermelon','orange']
+
+
+// unique items find using built in method
+let uniqueItems=[... new Set(array)]
+console.log('unique items',uniqueItems)
+
+
+// unique item found without built in method
+
+function foundUnique(arr){
+    let unique=[]
+    for(let i=0;i<arr.length;i++){
+    }
+}
